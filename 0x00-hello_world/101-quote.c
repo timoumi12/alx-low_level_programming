e63bc4758464db97a5d866da8ca4f049e215ad0a
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-20 01:55:20 +0100

    C hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ main.c eh whatever 
+ always 1
+ */
+int main (void)
+{
+  fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19",stdout);
+  return (0);
+}
