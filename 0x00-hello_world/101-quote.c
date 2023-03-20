37cfbc32cd9d14b46d5bf160f42b0036fae8c57c
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-20 02:02:24 +0100

    C hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 /**
- main.c eh whatever 
- always 1
+ * main - Entry point
+ *
+ * Description: Prints the message "and that piece of art is useful"
+ *              - Dora Korpar, 2015-10-19, followed by a new line,
+ *              to the standard error.
+ *              You are not allowed to use any functions listed in the
+ *              NAME section of the man (3) printf or man (3) puts.
+ *
+ * Return: 1 (Error)
  */
-int main (void)
+int main(void)
 {
-  fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19",stdout);
-  return (0);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
