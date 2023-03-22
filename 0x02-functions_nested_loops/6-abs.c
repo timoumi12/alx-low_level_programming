660a39d05bc1c3afb86f2e69fe37a33891d2be3e
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-22 15:04:29 +0100

    functions, nested_loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * _abs - whatever
@@ -10,24 +9,4 @@
 int _abs(int x)
 {
 	return (abs(x));
-}
-
-/**
- * main - eh
- *
- * Return: always 0
- */
-int main(void)
-{
-	int r;
-
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-	return (0);
 }
