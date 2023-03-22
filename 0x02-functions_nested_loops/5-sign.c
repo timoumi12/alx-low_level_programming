a5fc5b71fc4605c5112e99397b729aceac6def18
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-22 15:02:33 +0100

    functions, nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - whatever
@@ -8,54 +8,19 @@
  */
 int print_sign(int n)
 {
-	char a;
-
-	a = 'a';
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
-}
-
-/**
- * main - eh
- *
- * Return: always 0
- */
-int main(void)
-{
-	int r;
-
-	r = print_sign(98);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0xff);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(-1);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
 }
