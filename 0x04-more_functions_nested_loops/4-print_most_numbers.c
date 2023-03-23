60478d6bd92600d05b8a24d050d32547e8f7dbdf
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-23 23:27:43 +0100

    0x04fnl

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0~9
+ * print_most_numbers - print 0~9 except 2&4
  *
  * Return: void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-	if (i == 2 || i == 4)
 	{
-		continue;
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
 		_putchar(i + '0');
 	}
 	_putchar('\n');
