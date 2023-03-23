2abb68e675d6430cf57655eba946c429256656f8
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-23 22:11:06 +0100

    0x04fnl

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase
+ *
+ * @c: input
+ * Return: int
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
