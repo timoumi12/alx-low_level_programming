5e7f87333b0b40d28894c9b9d107789f0144d308
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-23 23:36:11 +0100

    0x04fnl

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0~14 x 10
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0;j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
