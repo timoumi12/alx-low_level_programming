a8fc37113bc2ecbf9fa4db138a16bd2ddce1be73
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-28 01:18:34 +0100

    pointers, arrays, strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - change value
+ * @s: string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = _strlen(s);
+	for (i = len - 1; i >= 0; i--)
+		printf("%c", s[i]);
+	printf("\n");
+}
