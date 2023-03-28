89ca49708f2fae3e903717bc04cdb93cee71627b
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-28 01:43:34 +0100

    pointers, arrays, strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,12 +12,13 @@ void print_rev(char *s)
 	int len;
 	int i;
 
+	len = 0;
 	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
 	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i] + '0');
+		_putchar(s[i]);
 	_putchar('\n');
 }
