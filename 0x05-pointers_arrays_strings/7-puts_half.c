a0a6aa34ef9a0fc7061239024834bc323f88d7fd
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-28 18:26:27 +0100

    pointers, arrays, strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints even caracters
+ * @str: string
+ *
+ * Return: always void
+*/
+void puts_half(char *str)
+{
+	int i;
+	char *p;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	str--;
+	if (i % 2 != 0)
+		while (i > (i - (i -1) / 2))
+		{
+			_putchar(*str);
+			str--;
+			i--;
+		}
+	else
+		while (i > i / 2)
+		{
+			_putchar(*str);
+			str--;
+			i--;
+		}
+}
