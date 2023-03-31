aabe520e5c1e967c2cc60914e57c195048c52729
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-31 12:51:28 +0100

    Pointers, arrays, strings 2.0

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,10 +16,11 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n, src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
 	}
+	dest[i + j] = '\0';
 	return (dest);
 }
 
