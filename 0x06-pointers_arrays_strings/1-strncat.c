23285b258b8c0381aec028cc26e755da5a0642f7
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-31 13:02:40 +0100

    Pointers, arrays, strings 2.0

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,24 +11,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-    char *start = dest;
-    
-    while (*dest != '\0')
-    {
-        dest++;
-    }
-    
-    while (n > 0 && *src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-        n--;
-    }
-    
-    *dest = '\0';
-    
-    return start;
+	char *start = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (n > 0 && *src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = '\0';
+	return (start);
 }
 
 
