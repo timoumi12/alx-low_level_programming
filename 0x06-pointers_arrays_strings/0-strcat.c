481bc0c5593f6d7d03c0840969eab2dd35c9a2a6
timoumi12 <ihebtimoumi1@gmail.com>
2023-03-31 12:24:24 +0100

    Pointers, arrays, strings 2.0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,21 +7,17 @@
  *
  * Return: string
 */
+
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
+	int i, j;
 
-	while (*dest != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest++;
-		i++;
+		dest[i + j] = src[j];
 	}
-	while (*src != '\0')
-	{
-		dest[i] = *src;
-		src++;
-		i++;
-	}
-	dest[i] = '\0';
+	dest[i + j] = '\0';
 	return (dest);
 }
+
