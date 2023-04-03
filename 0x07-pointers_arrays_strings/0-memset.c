ccae470c490818b1e06b87263ddb10cec37e8d0e
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-03 14:18:02 +0100

    pointers, arrays, strings 3.0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory
+ * @s: buffer
+ * @b: char
+ * @n: unsigned int
+ *
+ * Return: buffer
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers, arrays, strings 3.0
