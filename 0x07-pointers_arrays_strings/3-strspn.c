5a90766dff46cbf47b46a1e6550deccc66029b67
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-03 18:12:45 +0100

    pointers, arrays, strings 3.0

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i = 0;
 	int j;
 
-	while (*s != '\0')
+	while (*s)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
@@ -22,7 +22,7 @@ unsigned int _strspn(char *s, char *accept)
 				i++;
 				break;
 			}
-			else if (accept[j + 1] == '\0')
+			if (accept[j + 1] == '\0')
 				return (i);
 		}
 		s++;
