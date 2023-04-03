2efa60213265b9482631898283f7e35f951b5139
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-03 14:41:44 +0100

    pointers, arrays, strings 3.0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: buffer
+ * @src: src
+ * @n: unsigned int
+ *
+ * Return: buffer
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = src[i];
+	return (dest);
+}
+
