be4d8e019a2012f03e6a6718585042ee78cbc2bc
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-04 01:18:01 +0100

    pointers, arrays, strings 3.0

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - gets the length of a prefix substring
+ * @s: buffer
+ * @accept: char
+ *
+ * Return: int
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
+
