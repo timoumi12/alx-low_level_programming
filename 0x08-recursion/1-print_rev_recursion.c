ca545ae7e39d456861722371c7dee183708ff2e4
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-05 12:07:09 +0100

    recursion 1.0

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,12 +10,11 @@ void _print_rev_recursion(char *s)
 {
 	int i = 0;
 
-	if (*(s + i) == '\0')
+	if (*s == '\0')
 	{
 		return;
 	}
-	++i;
-	_print_rev_recursion(s + i);
-	_putchar(*(s + i));
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
 
