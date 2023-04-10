1fc8e0ae3907de213d0aa83177c9156370896e35
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-10 18:45:39 +0100

    static libraries

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  * Return: Always 0
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int __attribute__((__unused--)) argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
