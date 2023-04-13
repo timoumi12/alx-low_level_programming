fc2a8b94863cc3c404ef6d0ed80cedd358591c8d
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-14 00:38:05 +0100

    more malloc, free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: stirng
+ * @s2: string
+ * @n: unsigned int
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *res;
+	unsigned int i, j, x;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	res = malloc(sizeof(char) * (n + i + 1));
+	if (res == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+		res[j] = s1[j];
+	i = 0;
+	x = j;
+	while ((i < n) && (s2[i] != '\0'))
+	{
+		res[x] = s2[i];
+		i++;
+		x++;
+	}
+	res[x] = '\0';
+	return (res);
+}
+
