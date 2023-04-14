2a1b6bc99f24d221ebb43a000402385ff45bec08
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-14 02:10:32 +0100

    more malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,12 +9,12 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *res;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	char *res = malloc(nmemb * size);
-
+	res = malloc(nmemb * size);
 	if (res == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
