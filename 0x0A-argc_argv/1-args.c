ec61919d0277fca3407a21484dd34dcc7cfffe26
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-16 03:26:28 +0100

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - whatever
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+*/
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
