2d147bc6beeef8ad12f77534ff753536329ea1ce
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-17 17:45:39 +0100

    preprocessor

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,13 +3,18 @@
 
 /**
  * init_dog - check the code
- *
+ * @d: struct
+ * @name: string
+ * @age: float
+ * @owner: string
  * Return: Always void.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    (*d).name = name;
-    (*d).age = age;
-    (*d).owner = owner;
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
 
