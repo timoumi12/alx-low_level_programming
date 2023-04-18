ed74a10c639792f4dd3b8014f7b602bd9b823686
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-18 06:35:45 +0100

    structures, typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog
+ *
+ * @d: Pointer to the struct dog
+ * Return: void
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free((*d).name);
+	free((*d).owner);
+	free(d);	
+}
