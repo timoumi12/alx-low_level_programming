9ce07b95c6a369a88ba0127b65c95953ce00e9e4
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-18 07:03:06 +0100

    structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -63,7 +63,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
-	(*d).owner = malloc(_strlen(name) + 1);
+	(*d).owner = malloc(_strlen(owner) + 1);
 	if ((*d).owner == NULL)
 	{
 		free((*d).name);
