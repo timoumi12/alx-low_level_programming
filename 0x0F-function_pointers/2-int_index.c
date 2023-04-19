0993ed45cf9a8583fef926dcc41d010e9eead624
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-19 18:30:22 +0100

    function_pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search
+ * @array: array
+ * @size : size of array
+ * @cmp: pointer to a function
+ * Return: int
+ *
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			return (i);
+
+	return (-1);
+}
+
