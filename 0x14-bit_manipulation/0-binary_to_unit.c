9ea9152409b59a9e8140de57a73df145cff6d287
timoumi12 <ihebtimoumi1@gmail.com>
2023-05-04 22:33:28 +0100

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - binary to unit
+ * @b: char*
+ * Return: unsigned int
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int b10 = 0, p;
+	int n;
+
+	if (b == NULL)
+		return (0);
+
+	for (n = 0; b[n]; n++)
+	{
+		if (b[n] != '0' && b[n] != '1')
+			return (0);
+	}
+
+	for (p = 1, n--; n >= 0; n--, p *= 2)
+	{
+		if (b[n] == '1')
+			b10 += p;
+	}
+
+	return (b10);
+}
+
