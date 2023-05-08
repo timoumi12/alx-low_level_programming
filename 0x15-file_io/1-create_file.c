d6a4f3cc7e1c5023d42b3ee146636aaec3fdea0f
timoumi12 <ihebtimoumi1@gmail.com>
2023-05-08 11:31:08 +0100

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (o == -1)
 		return (o);
 	if (text_content != NULL)
