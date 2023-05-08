3ab8c4600f8794805aad4ffd3356aa22c82e10c1
timoumi12 <ihebtimoumi1@gmail.com>
2023-05-08 11:12:16 +0100

    file i/o

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - writes to file
+ * @filename: char*
+ * @text_content: char*
+ * Return: int
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t o, r, w, len;
+	char *dest;
+
+	if (filename == NULL)
+		return (-1);
+	o = open(filename, O_CREAT | O_WRONLY, 0666);
+	if (o == -1)
+		return (o);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	w = write(o, text_content, len);
+	if (w == -1)
+		return (0);
+	close(o);
+
+	return (w);
+}
+
