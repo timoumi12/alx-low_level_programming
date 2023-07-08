817fa636e68c056913c2af436dff58d2dffe673b
timoumi12 <ihebtimoumi1@gmail.com>
2023-07-08 12:36:28 +0100

    hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size
+ * Return: hash_table_t
+*/
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_table_t));
+	if (ht->array == NULL)
+		return (NULL);
+	for(i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	return (ht);
+}
