3b3cef23ea25b020798fa77542bedf0e3a4ae191
timoumi12 <ihebtimoumi1@gmail.com>
2023-07-08 12:55:52 +0100

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,10 +16,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 	ht->size = size;
-	ht->array = malloc(size * sizeof(hash_table_t));
+	ht->array = malloc(size * sizeof(hash_node_t *));
 	if (ht->array == NULL)
 	{
-		_free(ht);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
