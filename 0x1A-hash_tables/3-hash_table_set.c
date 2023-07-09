30af3a802c8eebb35e89411120b5d7467bac688d
timoumi12 <ihebtimoumi1@gmail.com>
2023-07-09 09:56:16 +0100

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,9 +10,8 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int i, index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *new;
-	char *dvalue;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
