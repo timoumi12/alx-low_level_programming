0ff65334ecb4c04a43a7a92b1826336e1ee85947
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-09 10:24:00 +0100

    first task

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Linear search algorithm
+ * @array: array
+ * @size: size_t
+ * @value: int
+ * Return: int
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if(array == NULL)
+		return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
